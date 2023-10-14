da027bd5992d50602e2d9b09e701ebe26621a8a2
vchemu <valerine.chemutai@student.moringaschool.com>
2023-10-14 04:41:04 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+  def __init__(self, word):
+    self.word = sorted([character for character in word])
+
+  def match(self, words):
+    match_list = []
+    for word in words:
+      if sorted([character for character in word]) == self.word:
+        match_list.append(word)
+    return match_list
